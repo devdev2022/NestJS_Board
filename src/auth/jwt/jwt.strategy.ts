09fa8dd9891313408c07334b82b7ee2174b59308
khs <devdev0226@gmail.com>
2023-08-19 19:04:19 +0900

    JWT 인증완료

---
+++ src/auth/jwt/jwt.strategy.ts
@@ -0,0 +1,16 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'secret',
+      ignoreExpiration: '1d',
+    });
+  }
+
+  //async validate(payload) {}
+}
